7b30f0fefa7b990e52681dfb9f7e79087acac462
felipe.palacios <felipe.palacios@pedidosya.com>
2020-04-27 15:40:35 -0300

    first commit

---
+++ spring-billing-liquidation-task-service/src/main/java/com/peya/billing/task/aws/SpringBillingLiquidationTaskServiceApplication.java
@@ -0,0 +1,29 @@
+package com.peya.billing.task.aws;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBillingLiquidationTaskServiceApplication implements CommandLineRunner{
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(SpringBillingLiquidationTaskServiceApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBillingLiquidationTaskServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		try {
+			Thread.sleep(5*1000);
+			LOGGER.info("LIQUIDATION: Task with argument {} was finished", args.toString());
+		} catch (InterruptedException e) {
+			LOGGER.info("LIQUIDATION: with argument  {} with error", args.toString(), e);
+			throw e;
+		}
+	}
+
+}
---
+++ spring-billing-liquidation-task-service/src/test/java/com/peya/billing/task/aws/SpringBillingLiquidationTaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peya.billing.task.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBillingLiquidationTaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
